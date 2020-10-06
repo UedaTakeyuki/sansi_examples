5341c1f0cfd3117180db04bdfea37398e9c6b177
Takeyuki UEDA <ueda@latelierdueda.com>
2020-10-06 09:47:44 +0900

    for mac

---
+++ c/compile.mac.sh
@@ -0,0 +1,19 @@
+# use openssl
+export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
+
+#
+# get your sansi library file name, remove "lib" prefix and ".a" postfix for linker 
+#
+original_libsansi=`ls libsansi*.a`
+end_removed=${original_libsansi%.a}
+libsansi=${end_removed#*lib}
+# echo $libsansi
+
+#
+# compile by clang with your sansi library
+#
+clang -o sansi main.c -L . -L /usr/local/opt/openssl/lib -lssl -lcrypto -lpthread -ldl -lm -l${libsansi}
+
+#
+# compile by gcc with your sansi library
+#gcc -o sansi main.c -L . -lssl -lcrypto -lpthread -ldl -lm -l${libsansi}
