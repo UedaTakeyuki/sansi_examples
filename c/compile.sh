45f779d44b1c153d1f8ee0dbf2db0dce9971980d
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2020-11-07 14:21:48 +0900

    merge 3 shell to compile.sh

--- c/compile.sh
+++ c/compile.sh
@@ -3,15 +3,31 @@
 # compile sample for Linux by clang
 
 usage_exit() {
-  echo "Usage: $0 [source] [libsansi]" 1>&2
+  echo "Usage: $0 [-h][-c][-g][-m] [source] [libsansi]" 1>&2
   echo "  [source]:   compiling source file, default is 'main.sample.c' "      1>&2
   echo "  [libsansi]: path for linking 'libsansi….o', default is found it in cwd automatically " 1>&2
+  echo "  [-h]: show this usage and exit" 1>&2
+
+  echo "  [-c]: compile for linux by clang" 1>&2
+  echo "  [-g]: compile for linux by gcc, this is default" 1>&2
+  echo "  [-m]: compile for mac by clang" 1>&2
   exit 1
 }
 
-while getopts "h" OPT; do
+compiler="gcc"
+
+while getopts "hcgm" OPT; do
 case "$OPT" in
   h) usage_exit
+     ;;
+  c) compiler="clang"
+     ;;
+  g) compiler="gcc"
+     ;;
+  m) compiler="clang"
+     # use openssl
+     export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
+     ;;
 esac
 done
 
@@ -29,14 +45,13 @@ fi
 
 echo "source   =" $sfile
 echo "libsansi =" $libsansi
+echo "compiler =" $compiler
 echo "compiling…"
 
 #
 # compile by clang with your sansi library
 #
-clang -o sansi $libsansi $sfile -L . -lssl -lcrypto -lpthread -ldl -lm
-
-echo "done."
+$compiler -o sansi $libsansi $sfile -L . -lssl -lcrypto -lpthread -ldl -lm
 
 # @author Dr. Takeyuki UEDA
 # @copyright Copyright© Atelier UEDA 2020 - All rights reserved.
