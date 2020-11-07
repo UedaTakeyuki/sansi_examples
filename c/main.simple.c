728df2e49947584b5587662ce2184f679b8aed4d
Takeyuki UEDA <ueda@latelierdueda.com>
2020-11-07 19:45:45 +0900

    10 sec count!

--- c/main.simple.c
+++ c/main.simple.c
@@ -1,12 +1,26 @@
-#include <stdio.h>
-#include "sansi.h"
+#include "sansi.h"  // for sansi libraries
+
+#include <stdio.h>  // for printf
+#include <unistd.h> // for seep
+void tenseccount();
 
 int main(){
 
   if (ok_confirmed == confirm(NULL, NULL, NULL)){
     printf("OK\n");
+    tenseccount();
   } else {
     printf("NG\n");
   }
   return 0;
+}
+
+void tenseccount(){
+  int i;
+  printf("start 10 second countdown\n");
+  for (i=10; i>0; i--){
+    printf("%d\n",i);
+    sleep(1);
+  }
+  printf("0!\n");
 }
