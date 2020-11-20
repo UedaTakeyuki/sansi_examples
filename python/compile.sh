6d3c63ff0898759b9eff49108168368ea2659a9a
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2020-11-20 22:43:34 +0900

    add comment for nuitka setting up

--- python/compile.sh
+++ python/compile.sh
@@ -1,3 +1,5 @@
 gcc -I/usr/include/python2.7/ -o sansiWrapper.o -c sansiWrapper.c
 gcc -shared libsansi_XMdvaoLeqDKn_arm_v1.1.o sansiWrapper.o -o sansimodule.so  -lssl -lcrypto -lpthread -ldl -lm
+
+# for nuitka setting up, refer http://www.nuitka.net/pages/download.html
 python -m nuitka --follow-imports --include-plugin-directory --plugin-enable=pylint-warnings main.py
